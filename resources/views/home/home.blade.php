e87fdbbc9d7d3a6726b5b3e159c18b88fe710a03
Emmanuel Gomez <emmagomez166@gmail.com>
2021-08-29 15:13:52 -0300

    website included

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,5 +1,5 @@
 @extends('base')
-@section('title') 25 Watts @endsection
+@section('title') 25 Watts API @endsection
 @section('content')
     <main class="container-fluid">
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
